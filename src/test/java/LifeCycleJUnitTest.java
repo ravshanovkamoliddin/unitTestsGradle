05e573c24255ef41f9b6adbc195c28d8deea25ec
Ravshanov Kamoliddin <rkamoliddin1991@gmail.com>
2025-05-11 11:06:54 +0500

    Add remote selenium

--- src/test/java/LifeCycleJUnitTest.java
+++ src/test/java/LifeCycleJUnitTest.java
@@ -8,25 +8,25 @@ public class LifeCycleJUnitTest {
 
     @BeforeEach
     void setUp() {
-        // Здесь размещаем код подготовки перед каждым тестом
+
         System.out.println("Перед каждым тестом");
     }
 
     @AfterEach
     void tearDown() {
-        // Здесь размещаем код очистки после каждого теста
+
         System.out.println("После каждого теста");
     }
 
     @BeforeAll
     void setUpAll() {
-        // Here we palace the preparation code before all tests
+
         System.out.println("Перед всеми тестами");
     }
 
     @AfterAll
     void tearDownAll() {
-        // Here we post the cleanup code after all tests
+
         System.out.println("After all the tests");
     }
 
