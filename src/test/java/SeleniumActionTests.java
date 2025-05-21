bc3ea84f4a884b48c03fd7257f66b8afd9830636
Ravshanov Kamoliddin <rkamoliddin1991@gmail.com>
2025-05-21 09:31:15 +0500

    upgrade gradle

--- src/test/java/SeleniumActionTests.java
+++ src/test/java/SeleniumActionTests.java
@@ -22,6 +22,8 @@ import static steps.WebFormSteps.sendInput;
 
 class SeleniumActionTests {
     WebDriver driver;
+    private String currentUrl;
+
     @BeforeEach
     void start() {
         driver = new ChromeDriver();
@@ -224,6 +226,7 @@ class SeleniumActionTests {
     }
 
     private void assertThat(String currentUrl) {
+        this.currentUrl = currentUrl;
     }
 
     @Test
